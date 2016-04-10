4d11629d85bf74cf7cb328aa6d88c34b6a0c6066
Zach Massia <zmassia@gmail.com>
2016-04-09 20:35:55 -0400

    Add script to copy Qt packages to virtualenv

---
+++ logging_app/link_pyqt.sh
@@ -0,0 +1,3 @@
+ln -s /usr/local/Cellar/pyqt/4.11.4/lib/python2.7/site-packages/PyQt4/ $WORKON_HOME/logging_app/lib/python2.7/site-packages/
+ln -s /usr/local/Cellar/pyqwt/5.2.0/lib/python2.7/site-packages/PyQt4/Qwt5/ $WORKON_HOME/logging_app/lib/python2.7/site-packages/PyQt4/
+ln -s /usr/local/Cellar/sip/4.17/lib/python2.7/site-packages/sip.so $WORKON_HOME/logging_app/lib/python2.7/site-packages/
